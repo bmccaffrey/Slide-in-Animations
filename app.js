eed26000c9a3291140b3bd7b3791f6b7585e437f
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-06-26 13:47:02 -0500

    allow images to slide out when no longer visible

--- app.js
+++ app.js
@@ -15,12 +15,33 @@ function debounce(func, wait = 20, immediate = true) {
 
 var images = document.querySelectorAll('img');
 
+
+
+// function slideImages() {
+//   images.forEach( image => {
+//     const slideInAt = (window.scrollY + window.innerHeight) - image.height / 2;
+//     const isHalfShown = slideInAt > image.offsetTop;
+//     if(isHalfShown){image.classList.add('active');}});}
+
+var x = images[0];
+
+
 function slideImages() {
-  images.forEach(img => {
-    if ( img.y <= 900 ) {
-      img.classList.add('active');
+  images.forEach (img => {
+    if ( img.y <= 780 ) {
+      img.classList.add( 'active' );
+    }
+    if ( img.y <= -img.height ) {
+      img.classList.remove( 'active');
     }
   });
 }
 
+function test() {
+  var x = images[0];
+  console.log(x.y);
+  
+}
+
 window.addEventListener( 'scroll', debounce( slideImages, 18 ));
+// window.addEventListener( 'scroll', test );
