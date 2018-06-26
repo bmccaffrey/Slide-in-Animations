ea83189678e75640bb13c1f0e53f170d1a67270b
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-06-26 13:50:22 -0500

    remove superfluous test

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var images = document.querySelectorAll('img');
+
 function debounce(func, wait = 20, immediate = true) {
   var timeout;
   return function() {
@@ -13,35 +15,11 @@ function debounce(func, wait = 20, immediate = true) {
   };
 }
 
-var images = document.querySelectorAll('img');
-
-
-
-// function slideImages() {
-//   images.forEach( image => {
-//     const slideInAt = (window.scrollY + window.innerHeight) - image.height / 2;
-//     const isHalfShown = slideInAt > image.offsetTop;
-//     if(isHalfShown){image.classList.add('active');}});}
-
-var x = images[0];
-
-
 function slideImages() {
   images.forEach (img => {
-    if ( img.y <= 780 ) {
-      img.classList.add( 'active' );
-    }
-    if ( img.y <= -img.height ) {
-      img.classList.remove( 'active');
-    }
+    if ( img.y <= 780 ) { img.classList.add( 'active' ); }
+    if ( img.y <= -img.height ) { img.classList.remove( 'active'); }
   });
 }
 
-function test() {
-  var x = images[0];
-  console.log(x.y);
-  
-}
-
 window.addEventListener( 'scroll', debounce( slideImages, 18 ));
-// window.addEventListener( 'scroll', test );
